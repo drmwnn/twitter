4ccb2bb075ee7447ebd77bd66a3bb873852a4411
drmwnn <asep.darmawan879@gmail.com>
2023-05-02 13:31:03 +0700

    display tweet

--- lib/features/tweet/widgets/hashtag_text.dart
+++ lib/features/tweet/widgets/hashtag_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter/theme/theme.dart';
 
 class HashtagText extends StatelessWidget {
   final String text;
@@ -11,7 +12,43 @@ class HashtagText extends StatelessWidget {
   Widget build(BuildContext context) {
     List<TextSpan> textspans = [];
 
-    text.split(' ').forEach((element) {});
-    return Container();
+    text.split(' ').forEach((element) {
+      if (element.startsWith('#')) {
+        textspans.add(
+          TextSpan(
+            text: '$element ',
+            style: const TextStyle(
+              color: Pallete.blueColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      } else if (element.startsWith('www.') || element.startsWith('https://')) {
+        textspans.add(
+          TextSpan(
+            text: '$element ',
+            style: const TextStyle(
+              color: Pallete.blueColor,
+              fontSize: 18,
+            ),
+          ),
+        );
+      } else {
+        textspans.add(
+          TextSpan(
+            text: '$element ',
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        );
+      }
+    });
+    return RichText(
+      text: TextSpan(
+        children: textspans,
+        ),
+      );
   }
 }
--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter/common/common.dart';
+import 'package:twitter/core/enums/tweet_type_enum.dart';
 import 'package:twitter/features/auth/controller/auth_controller.dart';
+import 'package:twitter/features/tweet/widgets/carousel_image.dart';
+import 'package:twitter/features/tweet/widgets/hashtag_text.dart';
 import 'package:twitter/models/tweet_model.dart';
 import 'package:twitter/theme/pallete.dart';
 import 'package:timeago/timeago.dart' as timeago;
+
 class TweetCard extends ConsumerWidget {
   final Tweet tweet;
   const TweetCard({
@@ -27,38 +31,44 @@ class TweetCard extends ConsumerWidget {
                         radius: 35,
                       ),
                     ),
-                    Column(
-                      children: [
-                        //retweeted
-                        Row(children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 5),
-                            child: Text(
-                              user.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 19,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //retweeted
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 19,
+                                  ),
+                                ),
                               ),
-                            ),
+                              Text(
+                                '@${user.name} . ${timeago.format(
+                                  tweet.tweetedAt,
+                                  locale: 'en_short',
+                                )}',
+                                style: const TextStyle(
+                                  color: Pallete.greyColor,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            '@${user.name} . ${timeago.format(
-                              tweet.tweetedAt,
-                              locale: 'en_short',
-                              )}',
-                            style: const TextStyle(
-                              color: Pallete.greyColor,
-                              fontSize: 17,
-                            ),
-                          ),
-                        ])
-                      ],
+                          //replied to
+                          HashtagText(text: tweet.text),
+                          if (tweet.tweetType == TweetType.image)
+                            CarouselImage(imageLinks: tweet.imageLinks),
+                        ],
+                      ),
                     ),
-                    //replied to
-                    
                   ],
                 ),
-                
               ],
             );
           },
