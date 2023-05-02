ee2b1952dea0c22cba074f12461f260cfd871093
drmwnn <asep.darmawan879@gmail.com>
2023-05-02 17:05:19 +0700

    icon coment, retweet, views

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,10 +1,13 @@
+import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter/common/common.dart';
+import 'package:twitter/constants/assets_constant.dart';
 import 'package:twitter/core/enums/tweet_type_enum.dart';
 import 'package:twitter/features/auth/controller/auth_controller.dart';
 import 'package:twitter/features/tweet/widgets/carousel_image.dart';
 import 'package:twitter/features/tweet/widgets/hashtag_text.dart';
+import 'package:twitter/features/tweet/widgets/tweet_icon_button.dart';
 import 'package:twitter/models/tweet_model.dart';
 import 'package:twitter/theme/pallete.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -23,6 +26,7 @@ class TweetCard extends ConsumerWidget {
             return Column(
               children: [
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
                       margin: const EdgeInsets.all(10),
@@ -64,6 +68,43 @@ class TweetCard extends ConsumerWidget {
                           HashtagText(text: tweet.text),
                           if (tweet.tweetType == TweetType.image)
                             CarouselImage(imageLinks: tweet.imageLinks),
+                          if (tweet.link.isNotEmpty) ...[
+                            const SizedBox(height: 4),
+                            AnyLinkPreview(
+                              displayDirection:
+                                  UIDirection.uiDirectionHorizontal,
+                              link: 'https://${tweet.link}',
+                            ),
+                          ],
+                          Container(
+                            margin: const EdgeInsets.only(
+                              top: 10,
+                              right: 20,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TweetIconButton(
+                                  pathName: AssetsConstants.viewsIcon,
+                                  text: (tweet.commentIds.length +
+                                          tweet.reshareCount +
+                                          tweet.likes.length)
+                                      .toString(),
+                                  onTap: () {},
+                                ),
+                                TweetIconButton(
+                                  pathName: AssetsConstants.commentIcon,
+                                  text: tweet.commentIds.length.toString(),
+                                  onTap: () {},
+                                ),
+                                TweetIconButton(
+                                  pathName: AssetsConstants.retweetIcon,
+                                  text: tweet.reshareCount.toString(),
+                                  onTap: () {},
+                                ),
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import device_info_plus
 import flutter_web_auth_2
 import package_info_plus
 import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import window_to_front
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
