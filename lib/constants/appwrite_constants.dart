b73222472e68a0738ccd5629f08b16cc4cb8b471
drmwnn <asep.darmawan879@gmail.com>
2023-05-08 21:58:35 +0700

    setup db

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,6 +1,6 @@
 class AppwriteConstants {
   static const String databaseId = '642d0acd67f4b3761ed1';
-  static const String projectId = '642d07c8109cf04bc8ea';
+  static const String projectId = '6458fdb21b4b5c726406';
   static const String endPoint = 'http://165.22.245.132/v1';
 
   static const String usersCollection = '643503943aff5133a06f';
